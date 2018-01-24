44ad0621b539b1e01561f2584d3c6df15b947155
MrFlyingChip <kv_l@ukr.net>
2018-01-24 21:46:51 +0200

    added project

---
+++ Touch Me/Assets/Scripts/CircleController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CircleController : MonoBehaviour {
+
+    public float timeToDeath;
+
+    public Vector3 scale;
+    private void Start()
+    {
+        GetComponent<RectTransform>().localScale = Vector3.zero;
+        StartCoroutine(CircleGrow());
+    }
+
+    IEnumerator CircleGrow()
+    {
+        while (scale.x < 1f)
+        {
+            scale = GetComponent<RectTransform>().localScale + new Vector3(0.01f, 0.01f, 0.01f);
+            GetComponent<RectTransform>().localScale = scale;
+            yield return new WaitForSeconds(timeToDeath * 0.01f);
+        }
+        DeleteCircle();
+    }
+
+    public void DeleteCircle()
+    {
+        Destroy(gameObject);
+    }
+	
+}
---
+++ Touch Me/Assets/Scripts/CircleSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CircleSpawner : MonoBehaviour {
+
+    public GameObject circlePrefab;
+    public Transform holder;
+    public RectTransform corner;
+    public Color[] circleColors;
+
+    public float minSpawnTime;
+    public float maxSpawnTime;
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartCoroutine(SpawnCircle());
+	}
+	
+	IEnumerator SpawnCircle()
+    {
+        while (true)
+        {
+            float spawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+            yield return new WaitForSeconds(spawnTime);
+            GameObject spawnedCircle = Instantiate(circlePrefab, holder) as GameObject;
+            spawnedCircle.GetComponent<RectTransform>().position = FindCirclePosition();
+            spawnedCircle.GetComponent<Image>().color = circleColors[Random.Range(0, circleColors.Length)];
+        }
+    }
+
+    Vector3 FindCirclePosition()
+    {
+        float x = Random.Range(0, corner.position.x);
+        float y = Random.Range(0, corner.position.y);
+        Debug.Log(x + " " + y);
+        return new Vector3(x, y, 0);
+    }
+}
